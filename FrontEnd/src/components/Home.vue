327debf4826a80acbb6e4a9dceb2ddf9951543e6
myhpham <57925032+myhpham@users.noreply.github.com>
2020-02-22 21:18:02 -0800

    front end

---
+++ FrontEnd/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="body">
+        <div id="title">
+            <h1>{{ home }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>export default {
+        name: 'Home',
+        props: {
+            home: String,
+            stats: String
+        }
+    }</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #title {
+        left: 100px
+    }
+
+    il {
+        font-size: 10px
+    }
+
+    h1 {
+        left: 100px;
+        color: #345235;
+    }
+</style>
